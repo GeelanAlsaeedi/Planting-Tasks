03dfb742513bb0a0e7b599d3f13ee7c197b2b2dd
Geelan <47663536+gogeebab@users.noreply.github.com>
2020-11-19 14:36:36 +0300

    Update PlantingTask.java

--- PlantingTask/src/plantingtask/PlantingTask.java
+++ PlantingTask/src/plantingtask/PlantingTask.java
@@ -23,6 +23,7 @@ public class PlantingTask extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
+                System.out.println("geelan ");
             }
         });
         
