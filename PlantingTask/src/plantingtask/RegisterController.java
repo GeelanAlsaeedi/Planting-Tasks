cea70870f639c295abf73edae5f67dc7b6dcb5b0
RaghadAlduais <48922647+RaghadAlduais@users.noreply.github.com>
2020-11-24 18:54:37 +0300

    editing

--- PlantingTask/src/plantingtask/RegisterController.java
+++ PlantingTask/src/plantingtask/RegisterController.java
@@ -5,9 +5,16 @@
  */
 package plantingtask;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,9 +23,16 @@ import javafx.fxml.Initializable;
  */
 public class RegisterController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+      public void changeScreenButton3Pushed(ActionEvent event) throws IOException{
+        Parent registerParent = FXMLLoader.load(getClass().getResource("Tasks.fxml"));
+        
+        Scene registerScene=new Scene(registerParent);
+        
+        Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(registerScene);
+        window.show();
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- PlantingTask/src/plantingtask/TasksController.java
+++ PlantingTask/src/plantingtask/TasksController.java
@@ -5,23 +5,54 @@
  */
 package plantingtask;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 /**
  * FXML Controller class
  *
  * @author Joman
  */
 public class TasksController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
+    
+ 
+    @FXML
+    private void MouseClick(MouseEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("Plant.fxml"));
+        Scene gameScene = new Scene(pane);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(gameScene);
+        window.show();
+    }
+     private void backButtonToTasks(ActionEvent event) throws IOException{
+         Parent registerParent = FXMLLoader.load(getClass().getResource("Tasks.fxml"));
+        
+        Scene registerScene=new Scene(registerParent);
+        
+        Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(registerScene);
+        window.show();
+    }
+     
+      @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    } 
+       
+     }
     
-}
+      
+    
+
