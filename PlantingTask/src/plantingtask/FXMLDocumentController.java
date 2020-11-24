7320985f4dc0c8ae84ceee2206f2cca8e5f8d7de
jumanafaraj <74708339+jumanafaraj@users.noreply.github.com>
2020-11-24 17:18:20 +0300

    connect Scene

--- PlantingTask/src/plantingtask/FXMLDocumentController.java
+++ PlantingTask/src/plantingtask/FXMLDocumentController.java
@@ -3,14 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package plantingproject;
+package plantingtask;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -25,6 +31,27 @@ public class FXMLDocumentController implements Initializable {
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+    }
+     public void changeScreenButtonPushed(ActionEvent event) throws IOException{
+        Parent loginParent = FXMLLoader.load(getClass().getResource("LogIn.fxml"));
+        
+        Scene loginScene=new Scene(loginParent);
+        
+        Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(loginScene);
+        window.show();
+    }
+     
+      public void changeScreenButton3Pushed(ActionEvent event) throws IOException{
+        Parent registerParent = FXMLLoader.load(getClass().getResource("Register.fxml"));
+        
+        Scene registerScene=new Scene(registerParent);
+        
+        Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(registerScene);
+        window.show();
     }
     
     @Override
