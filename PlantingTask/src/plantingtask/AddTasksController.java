6a33982b3b9e52872b93b3d959ffac158bb4ae71
RaghadAlduais <48922647+RaghadAlduais@users.noreply.github.com>
2020-11-29 01:03:12 +0300

    edit

--- PlantingTask/src/plantingtask/AddTasksController.java
+++ PlantingTask/src/plantingtask/AddTasksController.java
@@ -5,9 +5,17 @@
  */
 package plantingtask;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,9 +24,16 @@ import javafx.fxml.Initializable;
  */
 public class AddTasksController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+         @FXML
+    public void BackToTasks(ActionEvent event) throws IOException {
+        
+        Parent pane = FXMLLoader.load(getClass().getResource("Tasks.fxml"));
+        Scene gameScene = new Scene(pane);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(gameScene);
+        window.show();
+        
+    } 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
