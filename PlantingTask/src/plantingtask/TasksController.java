d8f10ff0fb973f313aa16fae2f6b6969056499a6
0r-a-n <gh.seyami@gmail.com>
2020-12-09 00:18:54 +0300

    Update TasksController.java

--- PlantingTask/src/plantingtask/TasksController.java
+++ PlantingTask/src/plantingtask/TasksController.java
@@ -22,6 +22,8 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
@@ -76,14 +78,20 @@ public class TasksController implements Initializable {
         window.show();
     }
 
-    @FXML
+    /*@FXML
     void toplant(MouseEvent event) throws IOException {
-        Parent registerParent1 = FXMLLoader.load(getClass().getResource("Plant.fxml"));
-        Scene registerScene1 = new Scene(registerParent1);
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("Plant.fxml"));
+        Parent plantPage = loader.load();
+        Scene WelcomeScene = new Scene(plantPage);
+        //access the controller and call a method
+        PlantController controller = loader.getController();
+        controller.initData(USER);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(registerScene1);
+        window.setScene(WelcomeScene);
         window.show();
-    }
+        
+    }*/
 
     public void initData(String userN) {
         USER = userN;
@@ -158,7 +166,217 @@ public class TasksController implements Initializable {
             }
         }
     }
-
+    
+     @FXML
+    void fromProcesstoTodyTasks(ActionEvent event) {
+      String str =Processing.getSelectionModel().getSelectedItem();
+      if(str != null){
+            Processing.getSelectionModel().clearSelection();
+            OBLWaitngTask.remove(str);
+            OBLTodayTask.add(str);
+        }
+         Session session = HibernateUtil.getSessionFactory().openSession();
+         session.beginTransaction();
+         List<Task_POJO> sList = null;
+         String queryStr = "from Task_POJO";
+         Query query = session.createQuery(queryStr);
+         sList = query.list();
+         
+        for (Task_POJO s : sList) {
+            if (USER.equals(s.getUserName()) && "Waiting".equals(s.getTaskState()) && str.equals(s.getTaskName())) {
+                s.setTaskState("Today");
+                session.update(s);
+                session.getTransaction().commit();
+                session.close();
+                break;
+            }
+        }
+    }
+    @FXML
+    void toAllTasks(ActionEvent event) {
+      String str =TodayTasks.getSelectionModel().getSelectedItem();
+      if(str != null){
+            TodayTasks.getSelectionModel().clearSelection();
+            OBLTodayTask.remove(str);
+            OBLAllTask.add(str);
+        }
+        Session session = HibernateUtil.getSessionFactory().openSession();
+         session.beginTransaction();
+         List<Task_POJO> sList = null;
+         String queryStr = "from Task_POJO";
+         Query query = session.createQuery(queryStr);
+         sList = query.list();
+         
+        for (Task_POJO s : sList) {
+            if (USER.equals(s.getUserName()) && "Today".equals(s.getTaskState()) && str.equals(s.getTaskName())) {
+                s.setTaskState("All");
+                session.update(s);
+                session.getTransaction().commit();
+                session.close();
+                break;
+            }
+        }
+    }
+    @FXML
+    void fromAllToTodayTasks(ActionEvent event) {
+      String str =AllTasks.getSelectionModel().getSelectedItem();
+      if(str != null){
+            AllTasks.getSelectionModel().clearSelection();
+            OBLAllTask.remove(str);
+            OBLTodayTask.add(str);
+        }
+        Session session = HibernateUtil.getSessionFactory().openSession();
+         session.beginTransaction();
+         List<Task_POJO> sList = null;
+         String queryStr = "from Task_POJO";
+         Query query = session.createQuery(queryStr);
+         sList = query.list();
+         
+        for (Task_POJO s : sList) {
+            if (USER.equals(s.getUserName()) && "All".equals(s.getTaskState()) && str.equals(s.getTaskName())) {
+                s.setTaskState("Today");
+                session.update(s);
+                session.getTransaction().commit();
+                session.close();
+                break;
+            }
+        }
+    }
+    
+    @FXML
+    void toProcessingTasks(ActionEvent event) {
+      String str =TodayTasks.getSelectionModel().getSelectedItem();
+      if(str != null){
+            TodayTasks.getSelectionModel().clearSelection();
+            OBLTodayTask.remove(str);
+            OBLWaitngTask.add(str);
+        }
+      Session session = HibernateUtil.getSessionFactory().openSession();
+         session.beginTransaction();
+         List<Task_POJO> sList = null;
+         String queryStr = "from Task_POJO";
+         Query query = session.createQuery(queryStr);
+         sList = query.list();
+         
+        for (Task_POJO s : sList) {
+            if (USER.equals(s.getUserName()) && "Today".equals(s.getTaskState()) && str.equals(s.getTaskName())) {
+                s.setTaskState("Waiting");
+                session.update(s);
+                session.getTransaction().commit();
+                session.close();
+                break;
+            }
+        }
+    }
+    
+    @FXML
+    void allTaskLV(KeyEvent event) {
+        String str =AllTasks.getSelectionModel().getSelectedItem();
+        
+        Session session = HibernateUtil.getSessionFactory().openSession();
+         session.beginTransaction();
+         List<Task_POJO> sList = null;
+         String queryStr = "from Task_POJO";
+         Query query = session.createQuery(queryStr);
+         sList = query.list();
+         
+        if(event.getCode()==KeyCode.RIGHT && str!= null){
+    
+            AllTasks.getSelectionModel().clearSelection();
+            OBLAllTask.remove(str);
+            OBLTodayTask.add(str);
+           
+           for (Task_POJO s : sList) {
+            if (USER.equals(s.getUserName()) && "All".equals(s.getTaskState()) && str.equals(s.getTaskName())) 
+            {   s.setTaskState("Today");
+                session.update(s);
+                session.getTransaction().commit();
+                session.close();
+                break;
+            }
+           }
+        }
+        AllTasks.requestFocus();
+    }
+    
+    @FXML
+    void todayLV(KeyEvent event) {
+        String str =TodayTasks.getSelectionModel().getSelectedItem();
+        
+        Session session = HibernateUtil.getSessionFactory().openSession();
+         session.beginTransaction();
+         List<Task_POJO> sList = null;
+         String queryStr = "from Task_POJO";
+         Query query = session.createQuery(queryStr);
+         sList = query.list();
+         
+        if(event.getCode()==KeyCode.LEFT && str!= null){
+    
+            TodayTasks.getSelectionModel().clearSelection();
+            OBLTodayTask.remove(str);
+            OBLAllTask.add(str);
+           
+           for (Task_POJO s : sList) {
+            if (USER.equals(s.getUserName()) && "Today".equals(s.getTaskState()) && str.equals(s.getTaskName())) 
+            {   s.setTaskState("All");
+                session.update(s);
+                session.getTransaction().commit();
+                session.close();
+                break;
+            }
+           }
+        }
+        if(event.getCode()==KeyCode.RIGHT && str!= null){
+    
+            TodayTasks.getSelectionModel().clearSelection();
+            OBLTodayTask.remove(str);
+            OBLWaitngTask.add(str);
+           
+           for (Task_POJO s : sList) {
+            if (USER.equals(s.getUserName()) && "Today".equals(s.getTaskState()) && str.equals(s.getTaskName())) 
+            {   s.setTaskState("Waiting");
+                session.update(s);
+                session.getTransaction().commit();
+                session.close();
+                break;
+            }
+           }
+        }
+        
+        TodayTasks.requestFocus();
+    }
+    
+    @FXML
+    void processingLV(KeyEvent event) {
+        String str =Processing.getSelectionModel().getSelectedItem();
+        
+        Session session = HibernateUtil.getSessionFactory().openSession();
+         session.beginTransaction();
+         List<Task_POJO> sList = null;
+         String queryStr = "from Task_POJO";
+         Query query = session.createQuery(queryStr);
+         sList = query.list();
+         
+        if(event.getCode()==KeyCode.LEFT && str!= null){
+    
+            Processing.getSelectionModel().clearSelection();
+            OBLWaitngTask.remove(str);
+            OBLTodayTask.add(str);
+           
+           for (Task_POJO s : sList) {
+            if (USER.equals(s.getUserName()) && "Waiting".equals(s.getTaskState()) && str.equals(s.getTaskName())) 
+            {   s.setTaskState("Today");
+                session.update(s);
+                session.getTransaction().commit();
+                session.close();
+                break;
+            }
+           }
+        }
+        Processing.requestFocus();
+    }
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
